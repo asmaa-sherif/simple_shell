7db906ffd76d6187d97ee4b09da7ecfd41a6b736
Ahmed Mohieldin <122824912+akamohi@users.noreply.github.com>
2023-04-29 22:29:43 +0200

    Update builtin_functions_1.c

--- builtin_functions_1.c
+++ builtin_functions_1.c
@@ -1,98 +1,121 @@
 #include "sample.h"
 
+int (*built_in_function[]) (char **) = { &my_cd, &my_help, &my_exit};
 /**
- * _myexit - exits the shell
- * @info: Structure containing potential arguments. Used to maintain
- * constant function prototype.
- * Return: exits with a given exit status
- * (0) if info.argv[0] != "exit"
+ * my_builtins - exit shell
+ *
+ * Return: size of builtin string
  */
-int _myexit(info_t *info)
+int my_builtins(void)
 {
-	int exitcheck;
-
-	if (info->argv[1]) /* If there is an exit arguement */
-	{
-		exitcheck = _erratoi(info->argv[1]);
-		if (exitcheck == -1)
-		{
-			info->status = 2;
-			print_error(info, "Illegal number: ");
-			_eputs(info->argv[1]);
-			_eputchar('\n');
-			return (1);
-		}
-		info->err_num = _erratoi(info->argv[1]);
-		return (-2);
-	}
-	info->err_num = -1;
-	return (-2);
+char *built_in_string[] = {"cd", "help", "exit"};
+return (sizeof(built_in_string) / sizeof(char *));
 }
-
 /**
- * _mycd - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
- * constant function prototype.
- * Return: Always 0
+ * my_help - help user
+ * @args: command input
+ *
+ * Return: 0 or 1
  */
-int _mycd(info_t *info)
+int my_help(char **args)
 {
-	char *s, *dir, buffer[1024];
-	int chdir_ret;
-
-	s = getcwd(buffer, 1024);
-	if (!s)
-		_puts("TODO: >>getcwd failure emsg here<<\n");
-	if (!info->argv[1])
-	{
-		dir = _getenv(info, "HOME=");
-		if (!dir)
-			chdir_ret = /* TODO: what should this be? */
-				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
-		else
-			chdir_ret = chdir(dir);
-	}
-	else if (_strcmp(info->argv[1], "-") == 0)
-	{
-		if (!_getenv(info, "OLDPWD="))
-		{
-			_puts(s);
-			_putchar('\n');
-			return (1);
-		}
-		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
-		chdir_ret = /* TODO: what should this be? */
-			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
-	}
-	else
-		chdir_ret = chdir(info->argv[1]);
-	if (chdir_ret == -1)
-	{
-		print_error(info, "can't cd to ");
-		_eputs(info->argv[1]), _eputchar('\n');
-	}
-	else
-	{
-		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
-		_setenv(info, "PWD", getcwd(buffer, 1024));
-	}
-	return (0);
+printf("This is simple C shell build by Komal Batool\n");
+printf("Type program names and arguments, and hit enter.\n");
+printf("%s is not a builtin function", args[0]);
+printf("Use the man command for information on other programs.\n");
+return (1);
 }
-
 /**
- * _myhelp - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
- * constant function prototype.
- * Return: Always 0
+ * execute - shell execution
+ * @args: input of shell
+ * @envp: environment of shell
+ *
+ * Return: 1 or launch funnc output
  */
-int _myhelp(info_t *info)
+int execute(char **args, char **envp)
 {
-	char **arg_array;
+int i;
+char *built_in_string[] = {"cd", "help", "exit"};
 
-	arg_array = info->argv;
-	_puts("help call works. Function not yet implemented \n");
-	if (0)
-		_puts(*arg_array); /* temp att_unused workaround */
-	return (0);
+if (args[0] == NULL)
+{
+return (1);
 }
+for (i = 0; i < my_builtins(); i++)
+{
+if (strcmp(args[0], built_in_string[i]) == 0)
+{
+return ((*built_in_function[i])(args));
+}
+}
+return (launch(args, envp));
+}
+/**
+ * read_line - read command
+ *
+ * Return: input command
+ */
+char *read_line(void)
+{
+    #ifdef USE_STD_GETLINE
+char *line = NULL;
+ssize_t bufsize = 0;
 
+if (getline(&line, &bufsize, stdin) == -1)
+{
+if (feof(stdin))
+exit(EXIT_SUCCESS);
+else
+{
+perror("can't read command.\n");
+exit(EXIT_FAILURE);
+}
+}
+return (line);
+#else
+int bufsize = RL_BUFSIZE;
+int position = 0;
+char *buffer = malloc(sizeof(char) * bufsize);
+if (!buffer)
+{
+fprintf(stderr, "allocation error\n");
+exit(EXIT_FAILURE);
+}
+while (1)
+{
+int c = getchar();
+if (c == EOF)
+exit(EXIT_SUCCESS);
+else if (c == '\n')
+{
+buffer[position] = '\0';
+return (buffer);
+}
+else
+buffer[position] = c;
+position++;
+while_loop(buffer, position);
+}
+#endif
+}
+/**
+ * while_loop - part of readline
+ * @position: position
+ * @buffer: buffer of input
+ *
+ * Return: void
+ */
+void while_loop(char *buffer, int position)
+{
+int bufsize = RL_BUFSIZE;
+if (position >= bufsize)
+{
+bufsize += RL_BUFSIZE;
+buffer = realloc(buffer, bufsize);
+if (!buffer)
+{
+fprintf(stderr, "allocation error\n");
+exit(EXIT_FAILURE);
+}
+}
+}
